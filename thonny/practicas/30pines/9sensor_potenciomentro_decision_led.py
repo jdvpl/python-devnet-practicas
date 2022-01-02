007d69aee973cabfd8ebf586b6b0de4ddb5d7233
JDVPL <juanda554242@gmail.com>
2022-01-02 16:19:18 -0500

    sensor con led desicion

--- thonny/practicas/30pines/9sensor_potenciomentro_decision_led.py
+++ thonny/practicas/30pines/9sensor_potenciomentro_decision_led.py
@@ -1,9 +1,10 @@
 from machine import Pin, ADC
 import utime
 
-#potenciomentro pin 36
-# se calibra deacuerdo a las especificaciones del sensor
+
 sensor=ADC(Pin(36))
+led_rojo=Pin(2,Pin.OUT)
+led_verde=Pin(4,Pin.OUT)
 #debe quedar atenua a 3v a 11db permite un rango de lectura entre 0v a 3.6v
 sensor.atten(ADC.ATTN_11DB)
 #colocar una resolucion de 12 bits
@@ -18,5 +19,11 @@ while True:
     lectura=sensor.read()
     factor_conversion=100/4096
     temp=factor_conversion*lectura
+    if temp >50:
+        led_rojo.value(1)
+        led_verde.value(0)
+    else:
+        led_verde.value(1)
+        led_rojo.value(0)
     print(f"{temp}°")
     utime.sleep_ms(100)
